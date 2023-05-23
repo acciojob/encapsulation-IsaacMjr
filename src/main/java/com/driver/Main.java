16ec0bcb90e4905fdba076c058446fa6662d9cf0
isaacMjr <magamboisaac9@gmail.com>
2023-05-23 12:18:47 +0300

    assignment commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public Main() {
+        RWOnly obj = new RWOnly();
+//        obj.name ="Isaac";
+        //the error displayed is name has a private access restriction in RWONly
+//        System.out.print(obj.name);
+        obj.setName("Isaac");
+        obj.getName();
+    }
 }
