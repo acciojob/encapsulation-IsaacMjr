0975a3c798fec083f86908c429542aed4113eb4e
isaacMjr <magamboisaac9@gmail.com>
2023-05-23 12:35:19 +0300

    corrected errors

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -3,11 +3,11 @@ package com.driver;
 public class RWOnly {
     private String name;
 
-    void setName(String name){
+   public void setName(String name){
         this.name = name;
     }
 
-    void getName(){
-        System.out.print( this.name);
+    public String getName(){
+        return this.name;
     }
 }
